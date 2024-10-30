6f6115c510bcae7b8c99738d499ce781aa92238d
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-30 16:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your code here
-// Select the button specifically and click
-cy.get('button.carousel-control-next').click(); // For next slide
-cy.get('button.carousel-control-prev').click(); // For previous slide
-
-// Verify the expected slide
-cy.get('.fourth-slide').should('be.visible'); // Example for fourth slide
-cy.get('.second-slide').should('be.visible'); // Example for second slide
